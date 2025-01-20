c8d2e145334cdf7d04f8b79d4ca39623c5301f5d
ludddo <mariani.ludovico.studente@itispaleocapa.it>
2025-01-20 10:45:44 +0100

    Aggiunti controlli UI e repository spese
    Sono stati aggiunti nuovi controlli all'interfaccia utente del form `DashboardForm` nel file `DashboardForm.Designer.cs`, inclusi una `Label`, un `DataGridView`, due `Button` e un `Panel`.
    
    È stato aggiunto un riferimento al namespace `Mariani_SpendWise.Data` nel file `DashboardForm.cs`.
    
    Nel file del progetto `Mariani_SpendWise.csproj`, è stato aggiunto il file `ExpenseRepository.cs` alla lista dei file compilati.
    
    È stato creato un nuovo file `ExpenseRepository.cs` con una classe statica `ExpenseRepository` che contiene i metodi `GetTotalExpenses` e `GetRecentExpenses` per interagire con il database delle spese.

---
+++ Mariani_SpendWise/Data/ExpenseRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using Mariani_SpendWise.Data;
+using MySql.Data.MySqlClient;
+
+namespace Mariani_SpendWise.Data
+{
+    public static class ExpenseRepository
+    {
+        public static decimal GetTotalExpenses()
+        {
+            string query = "SELECT SUM(amount) FROM expenses";
+            using (var conn = DatabaseHelper.GetConnection())
+            {
+                var cmd = new MySqlCommand(query, conn);
+                conn.Open();
+                object result = cmd.ExecuteScalar();
+                return result != DBNull.Value ? Convert.ToDecimal(result) : 0;
+            }
+        }
+
+        public static DataTable GetRecentExpenses()
+        {
+            string query = "SELECT date, category, amount, description FROM expenses ORDER BY date DESC LIMIT 10";
+            using (var conn = DatabaseHelper.GetConnection())
+            {
+                var cmd = new MySqlCommand(query, conn);
+                var adapter = new MySqlDataAdapter(cmd);
+                var table = new DataTable();
+                adapter.Fill(table);
+                return table;
+            }
+        }
+    }
+}
--- Mariani_SpendWise/Forms/DashboardForm.Designer.cs
+++ Mariani_SpendWise/Forms/DashboardForm.Designer.cs
@@ -28,20 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblTotalExpenses = new System.Windows.Forms.Label();
+            this.dgvRecentExpenses = new System.Windows.Forms.DataGridView();
+            this.btnAddExpense = new System.Windows.Forms.Button();
+            this.btnManageCategories = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRecentExpenses)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblTotalExpenses
+            // 
+            this.lblTotalExpenses.AutoSize = true;
+            this.lblTotalExpenses.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.lblTotalExpenses.Location = new System.Drawing.Point(50, 50);
+            this.lblTotalExpenses.Name = "lblTotalExpenses";
+            this.lblTotalExpenses.Size = new System.Drawing.Size(120, 21);
+            this.lblTotalExpenses.TabIndex = 0;
+            this.lblTotalExpenses.Text = "Totale Spese: €0";
+            // 
+            // dgvRecentExpenses
+            // 
+            this.dgvRecentExpenses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRecentExpenses.Location = new System.Drawing.Point(50, 100);
+            this.dgvRecentExpenses.Name = "dgvRecentExpenses";
+            this.dgvRecentExpenses.Size = new System.Drawing.Size(700, 200);
+            this.dgvRecentExpenses.TabIndex = 1;
+            // 
+            // btnAddExpense
+            // 
+            this.btnAddExpense.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            this.btnAddExpense.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnAddExpense.ForeColor = System.Drawing.Color.White;
+            this.btnAddExpense.Location = new System.Drawing.Point(50, 320);
+            this.btnAddExpense.Name = "btnAddExpense";
+            this.btnAddExpense.Size = new System.Drawing.Size(150, 35);
+            this.btnAddExpense.TabIndex = 2;
+            this.btnAddExpense.Text = "Aggiungi Spesa";
+            this.btnAddExpense.UseVisualStyleBackColor = false;
+            this.btnAddExpense.Click += new System.EventHandler(this.btnAddExpense_Click);
+            // 
+            // btnManageCategories
+            // 
+            this.btnManageCategories.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            this.btnManageCategories.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnManageCategories.ForeColor = System.Drawing.Color.White;
+            this.btnManageCategories.Location = new System.Drawing.Point(220, 320);
+            this.btnManageCategories.Name = "btnManageCategories";
+            this.btnManageCategories.Size = new System.Drawing.Size(200, 35);
+            this.btnManageCategories.TabIndex = 3;
+            this.btnManageCategories.Text = "Gestisci Categorie";
+            this.btnManageCategories.UseVisualStyleBackColor = false;
+            this.btnManageCategories.Click += new System.EventHandler(this.btnManageCategories_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 10);
+            this.panel1.TabIndex = 4;
             // 
             // DashboardForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 457);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnManageCategories);
+            this.Controls.Add(this.btnAddExpense);
+            this.Controls.Add(this.dgvRecentExpenses);
+            this.Controls.Add(this.lblTotalExpenses);
             this.Name = "DashboardForm";
             this.Text = "DashboardForm";
             this.Load += new System.EventHandler(this.DashboardForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRecentExpenses)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTotalExpenses;
+        private System.Windows.Forms.DataGridView dgvRecentExpenses;
+        private System.Windows.Forms.Button btnAddExpense;
+        private System.Windows.Forms.Button btnManageCategories;
+        private System.Windows.Forms.Panel panel1;
     }
 }
